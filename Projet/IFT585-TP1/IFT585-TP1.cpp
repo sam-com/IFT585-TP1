0794e1e2b7c730eb7d09d52b2ce8cd6bc45692e6
Michael Roy <roym1729@usherbrooke.ca>
2017-05-30 16:24:46 -0400

    Commencement de gabarit

--- Projet/IFT585-TP1/IFT585-TP1.cpp
+++ Projet/IFT585-TP1/IFT585-TP1.cpp
@@ -2,12 +2,71 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <thread>
 
 using namespace std;
 
-int main(int argc, char *argv[])
+size_t	tailleTempon;
+int		delaiTemporisation;
+string	fichierACopier;
+string	fichierDestination;
+
+
+void emission() {
+
+}
+
+void reception() {
+
+}
+
+void supportTransmission() {
+
+}
+
+int main()
 {
+	ifstream fichierParametres;
+	fichierParametres.open("parametres.txt");
+
+	string line;
+	if ( fichierParametres.is_open() ) {
+		if ( getline(fichierParametres, line) ) {
+			tailleTempon = atoi(line.c_str());
+		}
+
+		if (getline(fichierParametres, line)) {
+			delaiTemporisation = atoi(line.c_str());
+		}
+
+		if (getline(fichierParametres, line)) {
+			fichierACopier = line;
+		}
+
+		if (getline(fichierParametres, line)) {
+			fichierDestination = line;
+		}
+	}
+	else {
+		cout << "Fichier de paramètres introuvable.";
+	}
+
+	cout << "Taille du tempon: " << tailleTempon << endl
+		<< "Delai de temporisation (ms): " << delaiTemporisation << endl
+		<< "Fichier a copier: " << fichierACopier << endl
+		<< "Fichier de destination: " << fichierDestination << endl << endl;
+			
+	ifstream entree;
+	entree.open(fichierACopier);
+
+	if (entree.is_open()) {
+		
+	}
+	else {
+		cout << "Fichier a copier introuvable.";
+	}
 	cin.get();
     return 0;
 }
-
