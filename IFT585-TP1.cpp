7007543dc5361b5b30bc0b3cff612aae0cf07b6c
Cédric Bilodeau <bilc2603@usherbrooke.ca>
2017-06-09 13:55:44 -0400

    mise à jour de la thread transmission

--- IFT585-TP1.cpp
+++ IFT585-TP1.cpp
@@ -20,95 +20,64 @@ size_t	tailleTempon;
 int		delaiTemporisation;
 string	fichierACopier;
 string	fichierDestination;
-mutex m1;					// pour interdit l'ecriture en meme entre thread emission && supportTransmission
-mutex m2;					// pour interdit l'ecriture en meme entrethread reception && supportTransmission
-bool ready1 = false;		// thread emission a envoyer des donnees
-bool ready2 = false;		// thread reception a envoyer des donnees
-bool reception1 = false;	// reception disponible pour thread emission
-bool reception2 = false;	// reception disponible pour thread reception
-bitset<TrameTaille> b1;		// donnees entre thread emission && supportTransmission
-bitset<TrameTaille> b2;		// donnees entre thread reception && supportTransmission
-bool finiEmission;			
-bool finiEmissionTransmission;		
-bool finiReception;
+mutex mutexT1;		// pour interdit l'ecriture en meme entre thread emission && supportTransmission
+mutex mutexT3;		// pour interdit l'ecriture en meme entrethread reception && supportTransmission
 
 
-enum LogContexte { SEND, RECEIVE };
-void log(LogContexte LogContexte, bitset<TrameTaille> bits);
-string lireFichier(const string& nomFichier);
-void ecrireFichier(const string& nomFichier, const string& s);
-string extractTrame(string& fichier);
 
-void emission() {
-	string fichier = lireFichier(fichierACopier);
-	VectorCirculaire<bitset<TrameTaille>> vector(tailleTempon);
+enum EtatTransmission {
+	ENVOYER_T1, ENVOYER_T2, VIDE, UTILISER, PAS_UTILISER, FINI
+};
 
-	while (!fichier.empty()) {
-		bitset<TrameTaille> trame(string(extractTrame(fichier)));
-		
-		log(SEND, trame);
-		while (ready1) {
-			this_thread::sleep_for(std::chrono::seconds(1));
-		}
+struct Connection {
+	EtatTransmission& etat;
+	uint32_t trame;
+};
 
-		{
-			lock_guard<mutex> av(m1);
-			b1 = trame;
-			ready1 = true;
-		}
-	}
-	finiEmission = true;
+void emission(Connection connection) {
+
 }
 
-void reception() {
-	bitset<TrameTaille> reception;
-	string res;
-	while (!finiEmissionTransmission || reception2) {
-		while (!reception2) {
-			this_thread::sleep_for(std::chrono::seconds(1));
-		}
-		{
-			lock_guard<mutex> av(m2);
-			reception = b2;
-			reception2 = false;
-		}
-		log(RECEIVE, reception);
-		res += reception.to_string();
-	}
-	finiReception = true;
-	ecrireFichier(fichierDestination, res);
+void reception(Connection connection) {
+
 }
 
-void supportTransmission() {
-	bitset<TrameTaille> transport;
-	while (!ready1 && !ready2 && (!finiEmission || !finiReception)) {
+void supportTransmission(Connection& connectionT1,Connection& connectionT2) {
+	uint32_t transport; // variable temporaire utile juste pour l'insertion d'erreur
+	while (connectionT1.etat != ENVOYER_T1 || connectionT2.etat != ENVOYER_T2 || connectionT1.etat != FINI) {
 		this_thread::sleep_for(std::chrono::seconds(1));
-	}
-	if (ready1) {
-		{
-			lock_guard<mutex> av(m1);
-			transport = b1;
-		}
-		// TODO inserer des erreurs dans les bits 
+	} 
 
-		while (reception2) {
-			this_thread::sleep_for(std::chrono::seconds(1));
-		}
+	if (connectionT1.etat == ENVOYER_T1) {
+		connectionT1.etat = UTILISER;
 		{
-			lock_guard<mutex> av(m2);
-			b2 = transport;
-			reception2 = true;
+			lock_guard<mutex> av(mutexT1);
+			transport = connectionT1.trame;
 		}
-		if (finiEmission) {
-			finiEmissionTransmission = true;
+		// Insertion d'erreur ICI
+		{
+			lock_guard<mutex> av(mutexT3);
+			connectionT2.trame = transport;
 		}
-		ready1 = false;
-		supportTransmission();
-	} else if (ready2) {
-		// TODO
-		ready2 = false;
-		supportTransmission();
 	}
+	else if (connectionT1.etat == ENVOYER_T2) {
+		connectionT1.etat = UTILISER;
+		{
+			lock_guard<mutex> av(mutexT1);
+			transport = connectionT2.trame;
+		}
+		// Insertion d'erreur ICI
+		{
+			lock_guard<mutex> av(mutexT3);
+			connectionT1.trame = transport;
+		}
+	}
+	else {
+		// quand connection.etat == FINI
+		return;
+	}
+	connectionT1.etat = PAS_UTILISER;
+	supportTransmission(connectionT1,connectionT2);
 }
 
 int main()
@@ -137,49 +106,17 @@ int main()
 		<< "Fichier a copier: " << fichierACopier << endl
 		<< "Fichier de destination: " << fichierDestination << endl << endl;
 
-	std::thread th1(emission);
-	std::thread th2(supportTransmission);
+	EtatTransmission etat = VIDE;
 
-	reception();
+	Connection t1{ etat ,uint32_t {} }, t2{ etat , uint32_t {} };
+
+	std::thread th1(emission,t1);
+	std::thread th2(supportTransmission,t1,t2);
+
+	reception(t2);
 
 	th1.join();
 	th2.join();
 
 	system("pause");
-}
-
-string extractTrame(string& fichier) {
-	string retour;
-	if (fichier.size() == TrameTaille) {
-		retour = fichier;
-		fichier = "";
-	} else if (fichier.size() < TrameTaille) {
-		// TODO s'assurer de toujous avoir une trame de 32 bit
-	} else {
-		retour = fichier.substr(0, TrameTaille);
-		fichier = fichier.substr(TrameTaille + 1);
-	}
-	return retour;
-}
-
-string lireFichier(const string& nomFichier) {
-	string s;
-	copy(istreambuf_iterator<char>(std::ifstream{ nomFichier }),
-		istreambuf_iterator<char>{},
-		back_inserter(s));
-	return s;
-}
-
-void ecrireFichier(const string& nomFichier, const string& s) {
-	copy(begin(s),end(s),ostream_iterator<char>{ std::ofstream{ nomFichier } });
-}
-
-void log(LogContexte LogContexte, bitset<TrameTaille> bits) {
-	if (LogContexte == SEND) {
-		cout << "Send ";
-	}
-	else if (LogContexte == RECEIVE) {
-		cout << "Receive ";
-	}
-	cout << "Trame :" << bits << endl;
 }
